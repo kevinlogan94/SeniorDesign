1c211c973427d2411677a15516edff6ad3aa1f1c
Mandy Cox <MandeeD@users.noreply.github.com>
2016-03-19 17:07:33 -0400

    Create event.php
    Another outline file, because events are dependent on what was clicked. More cookie stuff I guess.

---
+++ event.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Individual Event Page</title>
+
+  <!--REQUIRED FOR HEADER-->
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script>$(function(){
+  $("#header").load("header.html"); });
+   </script>
+</head>
+<body>
+
+<!--REQUIRED FOR HEADER-->
+<div id="header"></div>
+
+<!-- List Event Title -->
+<!-- List Event Information -->
+
+</body>
+</html>
