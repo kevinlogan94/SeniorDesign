32d9d238c59ea3f4e2abd13e59d641247ad48ad0
Mandy Cox <MandeeD@users.noreply.github.com>
2016-03-19 17:00:42 -0400

    Create dashboard.php
    This is sort of an outline, because we need to know what user is logged in before we list their charities.

---
+++ dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Dashboard</title>
+
+  <!--REQUIRED FOR HEADER-->
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script>$(function(){
+  $("#header").load("header.html"); });
+   </script>
+</head>
+<body>
+
+<!--REQUIRED FOR HEADER-->
+<div id="header"></div>
+
+<!-- List User's name -->
+<!-- List charities according to owner -->
+
+</body>
+</html>
