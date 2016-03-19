1bdf1404f6327324d9c6e42daa3eba939680dcdd
Mandy Cox <MandeeD@users.noreply.github.com>
2016-03-19 17:08:54 -0400

    Create publicuserpage.php

---
+++ publicuserpage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Events of User</title>
+
+  <!--REQUIRED FOR HEADER-->
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script>$(function(){
+  $("#header").load("header.html"); });
+   </script>
+</head>
+<body>
+
+<!--REQUIRED FOR HEADER-->
+<div id="header"></div>
+
+<!-- List User Name -->
+<!-- List Events -->
+
+</body>
+</html>
