694feb438a18ee2f96809af5dbb8a8d61154a738
Kevin Logan <kmlo225@cs.uky.edu>
2016-04-08 11:45:25 -0400

    Date change

--- newcharity.php
+++ newcharity.php
@@ -42,7 +42,6 @@ $data = mysql_query("SELECT * FROM Tag");
 //insert header
  $(function(){
    $("#header").load("header.html"); });
-  
     /*
 	validateForm function
 	Purpose: Allow a user to submit their information or not based on whether the information is filled 
@@ -132,13 +131,76 @@ $data = mysql_query("SELECT * FROM Tag");
     Name:<p style="display:inline" id="Name"></p><br>
     <input type="text" name="Name" size="50"><br>
     Date(if applicable):<br>
-    <input type="text" name="Program Date" size="10"><br>
+    <select id="dropdowntextarea" name="month" size="1">
+        <option value="00">month</option>
+	<option value="01">January</option>
+    	<option value="02">February</option>
+    	<option value="03">March</option>
+    	<option value="04">April</option>
+    	<option value="05">May</option>
+    	<option value="06">June</option>
+    	<option value="07">July</option>
+   	<option value="08">August</option>
+    	<option value="09">September</option>
+    	<option value="10">October</option>
+    	<option value="11">November</option>
+    	<option value="12">December</option>
+    </select> / 
+    <select id="dropdowntextarea" style="margin-left: 0px;" name="day">
+	<option value="00">dd</option>    
+	<option value="01">01</option>
+    	<option value="02">02</option>
+    	<option value="03">03</option>
+    	<option value="04">04</option>
+    	<option value="05">05</option>
+    	<option value="06">06</option>
+    	<option value="07">07</option>
+    	<option value="08">08</option>
+    	<option value="09">09</option>
+    	<option value="10">10</option>
+    	<option value="11">11</option>
+    	<option value="12">12</option>
+    	<option value="13">13</option>
+    	<option value="14">14</option>
+    	<option value="15">15</option>
+    	<option value="16">16</option>
+    	<option value="17">17</option>
+    	<option value="18">18</option>
+    	<option value="19">19</option>
+    	<option value="20">20</option>
+    	<option value="21">21</option>
+    	<option value="22">22</option>
+    	<option value="23">23</option>
+    	<option value="24">24</option>
+    	<option value="25">25</option>
+    	<option value="26">26</option>
+    	<option value="27">27</option>
+    	<option value="28">28</option>
+    	<option value="29">29</option>
+    	<option value="30">30</option>
+    	<option value="31">31</option>
+    </select> / 
+    <select id="dropdowntextarea" style="margin-left: 0px;" name="year">
+	<option value="0000">yyyy</option>
+        <option value="2016">2016</option>
+	<option value="2017">2017</option>
+	<option value="2018">2018</option>
+	<option value="2019">2019</option>
+	<option value="2020">2020</option>
+	<option value="2021">2021</option>
+	<option value="2022">2022</option>
+	<option value="2023">2023</option>
+	<option value="2024">2024</option>
+	<option value="2025">2025</option>
+	<option value="2026">2026</option> 
+   </select>
+    <br>
     Address:<p style="display:inline" id="Address"></p><br>
     <input type="text" name="Address" size="50"><br>
     City: <p style="display:inline" id="City"></p><br>
     <input type="text" name="City" size="20"><br>
     State:<br>
-    <select id="dropdowntextarea" name = "type">
+    <select id="dropdowntextarea" name = "state">
 	<option value="AL">Alabama</option>
 	<option value="AK">Alaska</option>
 	<option value="AZ">Arizona</option>
